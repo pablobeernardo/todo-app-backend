d79bf32e45bfde0103f7754c63804da9a549c2d5
Pablo Bernardo <you@example.com>
2024-08-12 11:26:48 -0300

    atualizando pro neon Console

--- config/database.js
+++ config/database.js
@@ -1,14 +1,19 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize({
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  username: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+  port: 5432,
   dialect: 'postgres',
-  protocol: 'postgres',
   dialectOptions: {
     ssl: {
       require: true,
-      rejectUnauthorized: false, 
+      rejectUnauthorized: false,
     },
+    options: `project=${process.env.ENDPOINT_ID}`,
   },
   logging: console.log,
 });
